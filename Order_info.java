4237f27bf0643b6d206e4876f13dedc030e66605
PratyuHari <151715695+PratyuHari@users.noreply.github.com>
2024-04-12 16:28:15 +0530

    Add files via upload

---
+++ Order_info.java
@@ -0,0 +1,71 @@
+package com.cg.trg.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Order_info {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long orderId;
+	private String orderName;
+	private String email;
+	private boolean orderStatus;
+	private int quantity;
+	private String dateCreated;
+	public Order_info() {
+		super();
+	}
+	public Order_info(long orderId, String orderName, String email, boolean orderStatus, int quantity,
+			String dateCreated) {
+		super();
+		this.orderId = orderId;
+		this.orderName = orderName;
+		this.email = email;
+		this.orderStatus = orderStatus;
+		this.quantity = quantity;
+		this.dateCreated = dateCreated;
+	}
+	public long getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(long orderId) {
+		this.orderId = orderId;
+	}
+	public String getOrderName() {
+		return orderName;
+	}
+	public void setOrderName(String orderName) {
+		this.orderName = orderName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public boolean isOrderStatus() {
+		return orderStatus;
+	}
+	public void setOrderStatus(boolean orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public String getDateCreated() {
+		return dateCreated;
+	}
+	public void setDateCreated(String dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+	
+
+
+}
